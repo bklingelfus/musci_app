abe022bca107b96573c9523cd5a3aa916e1d201a
bklingelfus <bernardoklingelfus@gmail.com>
2022-11-26 00:22:03 -0300

    started on the css

--- public/jQuery/app.js
+++ public/jQuery/app.js
@@ -8,7 +8,33 @@ const refreshPlayer =()=>{
 
 // - - - - - - - - DOM - - - - - - - - - - -
 $(()=> {
+    // Changing Player
     $(document).ready(function(){
         $('.changeSong').on('click', refreshPlayer);
     });
+    // Player Functions
+    const $audio = document.getElementById("audio");
+    $audio.addEventListener('timeupdate', ()=>{
+        document.getElementById('tracktime').innerHTML = Math.floor($audio.currentTime) + ' / ' + Math.floor($audio.duration);
+    })
+    let volume = document.getElementById('volume-slider');
+    volume.addEventListener("change", function(e) {
+        $audio.volume = e.currentTarget.value / 100;
+    })
+    let playButton = document.getElementById('play-pause');
+    playButton.addEventListener("click", function() {
+        if($audio.paused){
+            $audio.play();
+        } else {
+            $audio.pause();  
+        }
+    })
+    let muteButton = document.getElementById('mute');
+    muteButton.addEventListener("click", function() {
+        if($audio.muted===true){
+            $audio.muted = false
+        } else {
+            $audio.muted=true 
+        }
+    })
 });
